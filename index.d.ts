7c16184bb102d40ac1398db3dc67cde2d1a167ae
Martin Heidegger <martin.heidegger@gmail.com>
2021-05-14 13:10:29 +0900

    types: proper default exprts

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
-declare module "extend-stack" {
+declare module extendStack {
   /**
    * Adds the error stack of the calling function in case an error occurs.
    * 
    * @param cb Callback to be wrapped.
    * @returns New callback that adds a stack if necessary.
    */
-  export function extendCb (cb: (err: Error | any, data: any) => void): ((err: Error, data: any) => void)
+  function extendCb (cb: (err: Error | any, data: any) => void): ((err: Error, data: any) => void)
   
   /**
    * Extends the stack with the current stack
@@ -14,5 +14,7 @@ declare module "extend-stack" {
    * @param offset (default=0) Additional lines to remove from the stack
    * @returns New error that contains the stack 
    */
-  export function extendStack(err: Error | any, offset?: number): Error
+  function extendStack(err: Error | any, offset?: number): Error
 }
+
+export = extendStack
